aee9bd3e092163e2ce96b3150c23262afa81adc0
MuketaUeda <gt.rosati@gmail.com>
2025-07-29 21:59:44 -0300

    index.css finalizado

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+export const Home = () => {
+    return (
+    <div className="min-h-screen">
+
+    {/*Theme*/}
+
+    {/*background*/}
+
+    {/*Navbar*/}
+
+    {/*Main*/}
+
+    {/*Footer*/}    
+
+
+    </div>
+    )
+}   
